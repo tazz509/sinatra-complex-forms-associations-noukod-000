82d73982959bd8942f51bc2feb3f874c57a7f2da
carly <carly@noukod.com>
2019-01-17 20:03:04 +0000

    Automatically backed up by Learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -6,19 +6,22 @@ class OwnersController < ApplicationController
   end
 
   get '/owners/new' do
+    @pets = Pet.all
     erb :'/owners/new'
   end
 
   post '/owners' do
-  @owner = Owner.create(params[:owner])
-  if !params["pet"]["name"].empty?
-    @owner.pets << Pet.create(name: params["pet"]["name"])
+     @owner = Owner.create(params[:owner])
+     if !params["pet"]["name"].empty?
+      @owner.pets << Pet.create(name: params["pet"]["name"])
+     end
+    redirect "owners/#{@owner.id}"
   end
-  redirect "owners/#{@owner.id}"
-end
+
 
   get '/owners/:id/edit' do
     @owner = Owner.find(params[:id])
+    @pets=Pet.all
     erb :'/owners/edit'
   end
 
@@ -40,11 +43,7 @@ end
       @owner.pets << Pet.create(name: params["pet"]["name"])
     end
     redirect "owners/#{@owner.id}"
-end
+  end
 
-  get '/owners/new' do
-    @pets = Pet.all
-    erb :'/owners/new'
- end
 
 end
